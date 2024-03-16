7198e46c265be16062272a17178eaefbc5a52def
mateuszmarc <132448647+mateuszmarc@users.noreply.github.com>
2024-03-16 01:00:30 +0000

    Revert "Edit"

--- src/main/java/pl/coderslab/dbmanagement/DbUtil.java
+++ src/main/java/pl/coderslab/dbmanagement/DbUtil.java
@@ -23,7 +23,7 @@ public class DbUtil {
             try {
                 Context initContext = new InitialContext();
                 Context envContext = (Context) initContext.lookup("java:/comp/env");
-                dataSource = (DataSource) envContext.lookup("jdbc/workshop3");
+                dataSource = (DataSource) envContext.lookup("jdbc/users");
             } catch (NamingException e) {
                 e.printStackTrace();
             }
